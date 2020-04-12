46df989035f56594044094d73c8e3cf3153a4eab
Logeswaran Audhikesavan <lokeshwaran.a82@gmail.com>
2020-04-12 10:24:31 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
    mongoURI: 'mongodb://localhost:27017/kerala',
-    //mongoURI:'mongodb+srv://manjappai:abcd1234@cluster0-96y3e.mongodb.net/test?retryWrites=true',
     secretOrKey: 'secret'
 }
